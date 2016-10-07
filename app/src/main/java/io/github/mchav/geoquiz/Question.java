a3718730f43d857406dbc49184695733b696a0e4
ChavXO <mchavinda@colgate.edu>
2016-10-07 12:29:33 -0400

    Initial commit

---
+++ app/src/main/java/io/github/mchav/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package io.github.mchav.geoquiz;
+
+/**
+ * Created by mscha on 07/10/2016.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
